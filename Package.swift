11966bcdd41be8d60df036fae27577410d615d57
wangzhizhou <wangzhizhou@bytedance.com>
2022-06-06 01:02:49 +0800

    swift package resolve

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/vapor/console-kit.git", from: "4.4.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.1"),
+        .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("5.5.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
