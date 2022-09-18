894bb4cfc703f5c0c74bc79688d4eca7a20a2ba0
wangzhizhou <wangzhizhou@bytedance.com>
2022-09-18 15:28:17 +0800

    format code

--- Sources/JokerKits/Shell.swift
+++ Sources/JokerKits/Shell.swift
@@ -12,6 +12,24 @@ public struct Shell {
     static let envPath = "/usr/bin/env"
     
     @discardableResult
+    public static func runCommand(with args: [String], workDirectory: String? = nil) throws -> String {
+        return try self.run(path: envPath, args: args, workDirectory: workDirectory)
+    }
+    
+    @discardableResult
+    public static func runCommand(with args: [String], workDirectory: String? = nil) async -> Bool {
+        return await self.run(path: envPath, args: args, workDirectory: workDirectory)
+    }
+    
+    @discardableResult
+    public static func runCommand(with args: [String], workDirectory: String? = nil, terminationHandler:((Process) -> Void)? = nil) throws -> Process {
+        return try self.run(path: envPath, args: args, workDirectory: workDirectory, terminationHandler: terminationHandler)
+    }
+}
+
+extension Shell {
+    
+    @discardableResult
     /// 同步执行Shell命令
     /// - Parameters:
     ///   - path: 命令二进制路径
@@ -24,41 +42,21 @@ public struct Shell {
         process.executableURL = URL(fileURLWithPath: path)
         process.arguments = args
         
-        if let workDirectory = workDirectory {
-            process.currentDirectoryURL = URL(fileURLWithPath: workDirectory)
-        }
-        
         let pipe = Pipe()
         process.standardOutput = pipe
         process.standardError = pipe
         
-        try process.run()
-        process.waitUntilExit()
-        
-        return String(data: pipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8)!
-    }
-    
-    @discardableResult
-    /// 异步执行Shell命令
-    /// - Parameters:
-    ///   - path: 命令二进制文件路径
-    ///   - args: 命令参数数组
-    ///   - terminationHandler: 执行结果回调
-    public static func run(path: String, args: [String], workDirectory: String? = nil, terminationHandler:((Process) -> Void)? = nil) throws -> Process {
-        let fileURL = URL(fileURLWithPath: path)
-        let process = Process()
-        process.executableURL = fileURL
-        process.arguments = args
         if let workDirectory = workDirectory {
             process.currentDirectoryURL = URL(fileURLWithPath: workDirectory)
         }
-        process.terminationHandler =  terminationHandler
-        try process.run()
         
-        return process
+        try process.run()
+        process.waitUntilExit()
+        
+        let result = String(data: pipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8)!
+        return result
     }
     
-
     @discardableResult
     public static func run(path: String, args: [String], workDirectory: String? = nil) async -> Bool {
         return await withCheckedContinuation({ continuation in
@@ -83,19 +81,24 @@ public struct Shell {
         })
     }
     
-    
     @discardableResult
-    public static func runCommand(with args: [String], workDirectory: String? = nil) async -> Bool {
-        return await self.run(path: envPath, args: args, workDirectory: workDirectory)
-    }
-    
-    @discardableResult
-    public static func runCommand(with args: [String], workDirectory: String? = nil, terminationHandler:((Process) -> Void)? = nil) throws -> Process {
-        return try self.run(path: envPath, args: args, workDirectory: workDirectory, terminationHandler: terminationHandler)
-    }
-    
-    @discardableResult
-    public static func runCommand(with args: [String], workDirectory: String? = nil) throws -> String {
-        return try self.run(path: envPath, args: args, workDirectory: workDirectory)
+    /// 异步执行Shell命令
+    /// - Parameters:
+    ///   - path: 命令二进制文件路径
+    ///   - args: 命令参数数组
+    ///   - terminationHandler: 执行结果回调
+    public static func run(path: String, args: [String], workDirectory: String? = nil, terminationHandler:((Process) -> Void)? = nil) throws -> Process {
+        let fileURL = URL(fileURLWithPath: path)
+        let process = Process()
+        process.executableURL = fileURL
+        process.arguments = args
+        
+        if let workDirectory = workDirectory {
+            process.currentDirectoryURL = URL(fileURLWithPath: workDirectory)
+        }
+        process.terminationHandler =  terminationHandler
+        try process.run()
+        
+        return process
     }
 }
