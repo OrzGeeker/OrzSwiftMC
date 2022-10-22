d29aceabe554ca35e0b4921a769c3f15b3a6aee2
wangzhizhou <824219521@qq.com>
2022-10-22 15:18:03 +0800

    add circle animatimated view

---
+++ client/OrzMC/Launcher/Views/CircleAnimationView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleAnimationView.swift
+//  OrzMC
+//
+//  Created by joker on 2022/10/22.
+//
+
+import SwiftUI
+
+struct CircleAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleAnimationView()
+    }
+}
