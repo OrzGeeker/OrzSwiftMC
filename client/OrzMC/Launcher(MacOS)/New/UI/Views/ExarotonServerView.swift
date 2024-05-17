65d555896841e6338e1d81ee95ec13acd06d77a8
wangzhizhou <wangzhizhou@bytedance.com>
2024-05-17 13:28:13 +0800

    add ExarotonServerView

---
+++ client/OrzMC/Launcher(MacOS)/New/UI/Views/ExarotonServerView.swift
@@ -0,0 +1,18 @@
+//
+//  ExarotonServerView.swift
+//  OrzMC
+//
+//  Created by joker on 2024/5/17.
+//
+
+import SwiftUI
+
+struct ExarotonServerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExarotonServerView()
+}
