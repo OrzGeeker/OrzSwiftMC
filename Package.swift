34f7df2d69c4063a4e9926ab87768457c4193d50
wangzhizhou <wangzhizhou@bytedance.com>
2024-06-27 15:29:09 +0800

    adjust swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.10
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -55,5 +55,5 @@ let package = Package(
             dependencies: ["Fabric"]
         ),
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageVersions: [.v6]
 )
