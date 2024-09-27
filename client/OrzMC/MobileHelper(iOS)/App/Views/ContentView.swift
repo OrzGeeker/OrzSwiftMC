b05ed6bdd702d9e11539e2c8f4cbd944efa9c58d
wangzhizhou <824219521@qq.com>
2024-09-27 22:28:57 +0800

    restore ios client logic

--- client/OrzMC/MobileHelper(iOS)/App/Views/ContentView.swift
+++ client/OrzMC/MobileHelper(iOS)/App/Views/ContentView.swift
@@ -9,20 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            
-            Tab("Client", systemImage: "macbook") {
-                Text("Client")
-            }
-            
-            Tab("Server", systemImage: "xserve") {
-                ExarotonContent()
-            }
-        }
+        ExarotonContent()
     }
 }
 
 #Preview {
     ContentView()
-        .environment(Model())
 }
