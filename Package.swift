b84c0b946bcd1f16ba68b471414f98a6988dc43a
wangzhizhou <wangzhizhou@bytedance.com>
2025-04-03 23:08:42 +0800

    update deps

--- Package.swift
+++ Package.swift
@@ -15,10 +15,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/OrzGeeker/OrzSwiftKit.git", from: "0.0.3"),
-        .package(url: "https://github.com/wangzhizhou/PaperMC.git", branch: "main"),
-//        .package(path: "/Users/bytedance/Documents/me/PaperMC"),
-        .package(url: "https://github.com/wangzhizhou/MojangAPI.git", from: "0.0.3"),
+        .package(url: "https://github.com/OrzGeeker/OrzSwiftKit.git", from: "0.0.12"),
+        .package(url: "https://github.com/wangzhizhou/PaperMC.git", from: "0.0.5"),
+        .package(url: "https://github.com/wangzhizhou/MojangAPI.git", from: "0.0.4"),
     ],
     targets: [
         // MARK: Command Line executable
